d00086d561367cf217e847d7588f39fc1d4a6b01
boyr23699-glitch <boyr23699@gmail.com>
2026-09-15 19:31:48 +0530

    Update api.py

--- api.py
+++ api.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify
 
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "status": "online",
+        "app": "NIFTY Market Direction App",
+        "direction": "WAIT"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
